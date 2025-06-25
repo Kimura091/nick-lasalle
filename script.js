88e687f6f20795be88c554f1551d69e93779217b
Kimura091 <nlasalle77@gmail.com>
2025-06-25 10:38:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("fs-menu").style.display = "block";
+    document.getElementById("openbutton").style.display = "none";
+}
+
+function closeNav(){
+    document.getElementById("fs-menu").style.display ="none";
+    document.getElementById("openbutton").style.display ="inline-block";
+}
